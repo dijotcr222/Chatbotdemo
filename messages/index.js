b0893e66499793e96a651e2fdb50b9a353f3e654
Anand <Anand@Anands-MacBook-Pro.local>
2017-06-24 06:41:14 +0530

    Changes

--- messages/index.js
+++ messages/index.js
@@ -39,6 +39,24 @@ var bot = new builder.UniversalBot(connector);
 bot.localePath(path.join(__dirname, './locale'));
 
 bot.dialog('/', function (session) {
+    var conn = new sql.Connection(connection);
+    var reqs = new sql.Request(conn);
+
+    conn.connect(function(err){
+      if(err){
+        console.log(err)
+      }else{
+        var SqlSt = "INSERT into ChatTable (ChatID, ChatMessage, localTime) VALUES";
+        SqlSt += util.format("(%d,%s,%s)", 454,"'"+session.message.text+"'","'"+session.message.localTimestamp+"'" );
+        reqs.query(SqlSt, function(err, data){
+            if(err){
+              console.log(err);
+            }else{
+              console.log("Saved")
+            }
+        });
+      }
+    });
     session.send('You said ' + session.message.text);
 });
 
