01623df927dd3b8d257bb96404db482a5065f8dd
Anand <Anand@Anands-MacBook-Pro.local>
2017-06-24 06:36:01 +0530

    Changes

--- messages/index.js
+++ messages/index.js
@@ -2,29 +2,8 @@
 var builder = require("botbuilder");
 var botbuilder_azure = require("botbuilder-azure");
 var path = require('path');
-var sql = require('mssql');
-var util = require('util');
 
 var useEmulator = (process.env.NODE_ENV == 'development');
-
-var connection = {
-    server: 'chattable.database.windows.net',
-    user: 'demo123',
-    password: 'D1j0=0kRia123',
-    database: 'ChatTable',
-    options: {
-	       encrypt: true
-	  }
-};
-
-sql.connect(connection, function (err) {
-  if(err){
-    console.log(err);
-    console.log("Error in connection");
-  }else{
-    console.log("DB Connected");
-  }
-})
 
 var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
     appId: process.env['MicrosoftAppId'],
@@ -37,23 +16,6 @@ var bot = new builder.UniversalBot(connector);
 bot.localePath(path.join(__dirname, './locale'));
 
 bot.dialog('/', function (session) {
-    var conn = new sql.Connection(connection);
-    var reqs = new sql.Request(conn);
-    conn.connect(function(err){
-      if(err){
-        console.log(err)
-      }else{
-        var SqlSt = "INSERT into ChatTable (ChatID, ChatMessage, localTime) VALUES";
-        SqlSt += util.format("(%d,%s,%s)", 45,"'"+session.message.text+"'","'"+session.message.localTimestamp+"'" );
-        reqs.query(SqlSt, function(err, data){
-            if(err){
-              console.log(err);
-            }else{
-              console.log("Saved")
-            }
-        });
-      }
-    });
     session.send('You said ' + session.message.text);
 });
 
